19d02fe4846fcad4791696803c2b15067b916f12
AlmanInc <sevhwarang@gmail.com>
2019-03-06 10:53:35 +0300

    Added animation by trigger script

---
+++ Parkour Runner/Assets/Scripts/Track/AnimationByTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AnimationByTrigger : BaseAnimatorController
+{
+    [SerializeField] private Animator[] _animators;
+
+    protected override void Init()
+    {
+        base.Init();
+
+        foreach (var animator in _animators)
+            animator.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == _player)
+        {
+            foreach (var animator in _animators)
+                animator.enabled = true;
+        }        
+    }
+}
