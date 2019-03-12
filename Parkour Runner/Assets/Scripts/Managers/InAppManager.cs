d4534a074cd5e2aff1c6c678d2aa03c62cf744d6
AlmanInc <sevhwarang@gmail.com>
2019-03-12 11:44:35 +0300

    Implemented InAppPurchases base variant

---
+++ Parkour Runner/Assets/Scripts/Managers/InAppManager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using UnityEngine.Purchasing;
+using UnityEngine;
+
+public class InAppManager : MonoBehaviour, IStoreListener
+{
+    [Serializable]
+    public class ProductConfiguration
+    {
+        public string gameId;
+        public string appleStoreId;
+        public string googlePlayId;
+        public ProductType productType;
+        public DonatShopData data;
+    }
+
+    public static event Action<ProductConfiguration> OnBuySuccess;
+
+    private static IStoreController StoreController { get; set; }
+    private static IExtensionProvider StoreExtensionProvider { get; set; }
+
+    [SerializeField] private ProductConfiguration[] _products;
+    
+    private void Awake()
+    {
+        if (!IsInitialized())
+        {
+            var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+            foreach (ProductConfiguration product in _products)
+            {
+                builder.AddProduct(product.gameId, product.productType, new IDs() { { product.appleStoreId, AppleAppStore.Name }, { product.googlePlayId, GooglePlay.Name } });
+            }
+            
+            UnityPurchasing.Initialize(this, builder);
+        }
+    }
+
+    public bool IsInitialized()
+    {
+        return StoreController != null && StoreExtensionProvider != null;
+    }
+    
+    /// <summary>
+    /// IOS only!
+    /// </summary>
+    private void RestorePurchases()
+    {
+        if (!IsInitialized())
+        {
+            Debug.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+
+        if (Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            var apple = StoreExtensionProvider.GetExtension<IAppleExtensions>();
+            apple.RestoreTransactions((result) =>
+            {
+                Debug.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore.");
+            });
+        }
+        else
+            Debug.Log("RestorePurchases FAIL. Not supported on this platform. Current = " + Application.platform);
+
+    }
+
+    public void BuyProductID(string productId)
+    {
+        try
+        {
+            if (IsInitialized())
+            {
+                Product product = StoreController.products.WithID(productId);
+                
+                if (product != null && product.availableToPurchase)
+                {
+                    Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));// ... buy the product. Expect a response either through ProcessPurchase or OnPurchaseFailed asynchronously.
+                    StoreController.InitiatePurchase(product);
+                }
+                else
+                {
+                    Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+                }
+            }
+            else
+            {
+                Debug.Log("BuyProductID FAIL. Not initialized.");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("BuyProductID: FAIL. Exception during purchase. " + e);
+        }
+    }
+
+    public bool CheckNonConsumable(string id)
+    {
+        return IsInitialized() && StoreController.products.WithID(id).hasReceipt;
+    }
+
+    public string GetLocalizedPrice(string id)
+    {
+        return IsInitialized() ? StoreController.products.WithID(id).metadata.localizedPriceString : string.Empty;
+    }
+
+    public string GetLocalizedCurrency(string id)
+    {
+        return IsInitialized() ? StoreController.products.WithID(id).metadata.isoCurrencyCode : string.Empty;
+    }
+
+    #region IStoreListener
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        StoreController = controller;
+        StoreExtensionProvider = extensions;
+
+        RestorePurchases();
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("OnInitializeFailed with error: " + error);
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        foreach (ProductConfiguration item in _products)
+        {
+            if (item.gameId == e.purchasedProduct.definition.id)
+            {
+                OnBuySuccess.SafeInvoke(item);
+                break;
+            }
+        }
+        
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnPurchaseFailed(Product i, PurchaseFailureReason p)
+    {
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", i.definition.storeSpecificId, p));
+    }
+    #endregion
+}
