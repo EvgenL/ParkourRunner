6c2734ec0498d8a6bc43d437bb4354a4a5ea06ed
AlmanInc <sevhwarang@gmail.com>
2019-03-26 12:22:48 +0300

    Updated characters graphics (not finished)

--- Parkour Runner/Assets/Scripts/MainMenuAndShop/Character/CharacterSelection.cs
+++ Parkour Runner/Assets/Scripts/MainMenuAndShop/Character/CharacterSelection.cs
@@ -4,6 +4,8 @@ using AEngine;
 public class CharacterSelection : MonoBehaviour
 {
     [SerializeField] private CharacterKinds _kind;
+    [SerializeField] private RectTransform _avatar;
+    [SerializeField] private RectTransform _avatarPivot;
 
     public void OnSelectButtonClick()
     {
@@ -11,5 +13,12 @@ public class CharacterSelection : MonoBehaviour
         PlayerPrefs.Save();
 
         AudioManager.Instance.PlaySound(Sounds.Bonus);
+    }
+
+    private void Update()
+    {
+        Vector3 position = _avatar.position;
+        position.x = _avatarPivot.position.x;
+        _avatar.position = position;
     }
 }
