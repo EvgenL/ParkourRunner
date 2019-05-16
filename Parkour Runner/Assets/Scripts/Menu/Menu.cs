4819f679966c3bab5a12eebe12f2a4e62acf9d8a
AlmanInc <sevhwarang@gmail.com>
2019-05-16 11:34:07 +0300

    Menu update preparation

---
+++ Parkour Runner/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public enum MenuKinds
+{
+    None,
+    MainMenu,
+    Shop
+}
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject _menuObject;
+    [SerializeField] private MenuKinds _kind;
+
+    private MenuController _menuController;
+
+    public bool IsActive { get { return _menuObject.activeSelf; } set { _menuObject.SetActive(value); } }
+
+    public void Init(MenuController controller)
+    {
+        if (Application.isPlaying)
+        {
+            if (_menuObject == null)
+                _menuObject = this.gameObject;
+
+            _menuController = controller;
+            this.IsActive = false;
+
+            controller.OnShowMenu += OnShowMenu;
+            controller.OnHideMenu += OnHideMenu;
+        }
+    }
+
+    protected virtual void Show()
+    {
+        _menuController.CurrentMenu = this;
+        this.IsActive = true;
+    }
+
+    protected virtual void Hide(Action callback)
+    {
+        if (_menuController.CurrentMenu == this)
+            _menuController.CurrentMenu = null;
+
+        callback.SafeInvoke();
+        this.IsActive = false;
+    }
+
+    #region Events
+    private void OnShowMenu(MenuKinds kind)
+    {
+        if (!this.IsActive && _kind == kind)
+            Show();
+    }
+
+    private void OnHideMenu(MenuKinds kind, Action callback)
+    {
+        if (this.IsActive && _kind == kind)
+            Hide(callback);
+    }
+    #endregion
+}
---
+++ Parkour Runner/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public static MenuKinds TransitionTarget { get; set; }
+
+    public event Action<MenuKinds> OnShowMenu;
+    public event Action<MenuKinds, Action> OnHideMenu;
+
+    [SerializeField] private MenuKinds _defaultMenu;
+    [SerializeField] private Menu[] menus;
+
+    public Menu CurrentMenu { get; set; }
+
+    private void Awake()
+    {
+        foreach (Menu item in menus)
+            item.Init(this);
+
+        OnShowMenu.SafeInvoke(TransitionTarget == MenuKinds.None ? _defaultMenu : TransitionTarget);
+    }
+}
