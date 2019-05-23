ea4bfeb3cc7b394c73c79f3fe862012f16be3ef2
AlmanInc <sevhwarang@gmail.com>
2019-05-23 16:31:10 +0300

    Finished shop UI updating, updated code structure for it

--- Parkour Runner/Assets/Scripts/MainMenuAndShop/Bonuses/BonusesShopData.cs
+++ Parkour Runner/Assets/Scripts/MainMenuAndShop/Bonuses/BonusesShopData.cs
@@ -14,10 +14,21 @@ public class BonusesShopData : ScriptableObject
     [SerializeField] private IconData[] _icons;
     [SerializeField] private BonusName _bonusKind;
     [SerializeField] private int[] _prices;
+    [SerializeField] private string _description;
     
-    public IconData[] Icons { get { return _icons; } }
-    
+    public IconData[] Icons { get { return _icons; } }    
     public BonusName BonusKind { get { return _bonusKind; } }
-
     public int[] Prices { get { return _prices; } }
+    public string Description { get { return _description; } }
+
+    public Sprite GetActualIcon(int progress)
+    {
+        for (int i = 0; i < _icons.Length; i++)
+        {
+            if (progress <= _icons[i].progressPower)
+                return _icons[i].icon;
+        }
+
+        return _icons[0].icon;
+    }
 }
