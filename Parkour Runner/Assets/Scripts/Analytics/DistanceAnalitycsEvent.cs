4a93450443c9c4869c32d7cdc9f0ecedf461f2dc
AlmanInc <sevhwarang@gmail.com>
2019-07-24 17:12:49 +0300

    Implemented some analitycs events, Unity Analitycs integration finished.

---
+++ Parkour Runner/Assets/Scripts/Analytics/DistanceAnalitycsEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using ParkourRunner.Scripts.Managers;
+
+public class DistanceAnalitycsEvent : MonoBehaviour
+{
+    [SerializeField] private AnalyticsEventTracker _analitycsEvent;
+
+    public float Distance { get; set; }
+
+    private void OnEnable()
+    {
+        this.Distance = GameManager.Instance.DistanceRun;
+        _analitycsEvent.TriggerEvent();
+    }
+}
